483b8461f53806acd4b3c972c1dc7b3f759941e3
我 <396953968@qq.com>
2015-10-31 10:24:32 +0800

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by 李新奇 on 15/10/31.
+//  Copyright © 2015年 李新奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by 李新奇 on 15/10/31.
+//  Copyright © 2015年 李新奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
